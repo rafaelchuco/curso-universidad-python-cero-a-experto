c38382f49f39a694ad38a33fd7e0722c83a62240
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-05-08 20:44:36 -0500

    add indices en cadenas

---
+++ o4. Manejo de Cadenas en Python/indices_cadenas.py
@@ -0,0 +1,11 @@
+# Manejo de indice en una cadena
+cadena1 = "Hola Mundo"
+
+print(cadena1)
+
+# Recuperar el primer caracter
+primer_caracter = cadena1[0]
+print(primer_caracter)
+
+ultimo_caracter = cadena1[-1]
+print(ultimo_caracter)
