454b6034ee642bf41d523c8ebf19d4171f85c950
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-05-22 17:58:42 -0500

    Add concatenacion_cadenas.py

---
+++ o4. Manejo de Cadenas en Python/concatenacion_cadenas.py
@@ -0,0 +1,15 @@
+# concatenacion de cadenas
+
+cadena1 = "Hola"
+cadena2 = "mundo"
+concatenacion = cadena1 + " " + cadena2
+
+print(concatenacion)
+
+# Utilizando el metodo join
+
+cadena1 = "Adios"
+cadena2 = "mundo"
+
+concatenacion = "".join([cadena1, " ", cadena2])
+print(concatenacion)
