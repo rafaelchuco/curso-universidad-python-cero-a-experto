066a79c7cd86a510f015ec8920d86276a8b8beab
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-05-22 17:57:59 -0500

    Add caracteres_especiales.py

---
+++ o4. Manejo de Cadenas en Python/caracteres_especiales.py
@@ -0,0 +1,6 @@
+# Caracteres Especiales
+print("Hola, \nMundo") # \n salto de linea
+print("\t Python es genial") # \t tabulador
+print('Juan \' Perez\' ')
+print("karla \" Gomez")
+print("Caracter \\ diagonal invertida")
