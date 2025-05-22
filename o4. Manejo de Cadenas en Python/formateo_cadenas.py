ec7b1e9c74b25019edfb915b7c5ee1ef542432e7
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-05-22 17:58:58 -0500

    Add formateo_cadenas.py

---
+++ o4. Manejo de Cadenas en Python/formateo_cadenas.py
@@ -0,0 +1,12 @@
+# Formateo de cadenas
+
+nombre = "Juan"
+edad = 25
+
+# f-string
+mensaje = f"Hola {nombre}, tienes {edad} años"
+print(mensaje)
+
+# format
+mensaje = "Hola {}, tienes {} años".format(nombre, edad)
+print(mensaje)
