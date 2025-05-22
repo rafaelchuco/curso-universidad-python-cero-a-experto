94c27a520eb82524d7758ed7137c35b2b3290c7f
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-05-22 18:08:45 -0500

    Add buscar_subcadenas.py

---
+++ o4. Manejo de Cadenas en Python/buscar_subcadenas.py
@@ -0,0 +1,8 @@
+# Buscar subcadenas
+
+cadena = "Hola, mundo"
+indice = cadena.find("mundo") # Obtener el indice de mundo
+print(indice)
+
+indice = cadena.find("Hola") # Obtener el indice de Hola
+print(indice)
