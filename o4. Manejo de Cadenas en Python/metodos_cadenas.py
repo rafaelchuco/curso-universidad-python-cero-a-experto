4b3aa8cca6d025d4c0f022feca536a574fefc642
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-05-22 18:00:25 -0500

    Add metodos_cadenas.py

---
+++ o4. Manejo de Cadenas en Python/metodos_cadenas.py
@@ -0,0 +1,17 @@
+# Metodos de cadenas
+
+cadena1 = "Hola, mundo"
+print(f"Cadena original: {cadena1} ")
+
+mayusculas = cadena1.upper() # Convierte a mayusculas
+print(f"Cadena en mayusculas: {mayusculas}")
+
+minusculas = cadena1.lower() # Convierte a minusculas
+print(f"Cadena en minusculas: {minusculas}")
+
+cadena2 = "  Adios, mundo  "
+print(f"Cadena con espacios: {cadena2} ")
+
+sin_espacios = cadena2.strip() # Elimina los espacios al principio y al final
+print(f"Cadena sin espacios: {sin_espacios}")
+
