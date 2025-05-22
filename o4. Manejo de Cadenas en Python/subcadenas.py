9d46b4111c361f11e9db4eb97875cbd395527f41
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-05-22 18:00:38 -0500

    Add subcadenas.py

---
+++ o4. Manejo de Cadenas en Python/subcadenas.py
@@ -0,0 +1,10 @@
+# Manejo de subcadenas
+cadena = "Hola, mundo!"
+
+# Obtenemos la subcandena de hola [inicio:fin]
+subcadena_hola = cadena[0:4]
+print(subcadena_hola)
+
+# Obtenemos la subcadena de mundo [inicio:fin]
+subcadena_mundo = cadena[6:11]
+print(subcadena_mundo)
