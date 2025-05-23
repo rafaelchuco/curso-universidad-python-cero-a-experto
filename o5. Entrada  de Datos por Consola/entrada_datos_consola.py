d8c9d11b9fbc047825007be53db079158f4b3d73
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-05-22 19:34:44 -0500

    Add entrada_datos_consola.py

---
+++ o5. Entrada  de Datos por Consola/entrada_datos_consola.py
@@ -0,0 +1,5 @@
+# Entrada de datos por consola
+
+nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad: "))
+print(f"Hola {nombre}, tienes {edad} años y en un año tendrás {edad + 1} años")
