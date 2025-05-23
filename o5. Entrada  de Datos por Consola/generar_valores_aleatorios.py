fdbe3042c56c64b8a0f0546bdf8c7d5f21cc6834
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-05-22 20:08:46 -0500

    Add generar_valores_aleatorios.py

---
+++ o5. Entrada  de Datos por Consola/generar_valores_aleatorios.py
@@ -0,0 +1,13 @@
+# Generar valores aleatorios numericos
+
+# import random
+from random import randint
+
+# Generar un numero aleatorio entre 1 y 10
+generar_numeros = randint(1, 10)
+
+print("Numero aleatoeio entre 1 y 10: ", generar_numeros)
+
+# Simular un dado de 6 lados
+generar_dado = randint(1, 6)
+print("resultado del dado: ", generar_dado)
