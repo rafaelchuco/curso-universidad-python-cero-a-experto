7a68f51f0cad8354b7af3f6e2eef18eea0d7866f
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-05-22 19:33:02 -0500

    Add ejemplo_tipos_datos.py

---
+++ o5. Entrada  de Datos por Consola/ejemplo_tipos_datos.py
@@ -0,0 +1,13 @@
+# Ejemplo de tipos de datos
+
+# Ejemplo de concatenación o suma de valores
+numero_cadena1 = "10"
+print("El valor 1 es: " + str(numero_cadena1) + "")
+numero_cadena2 = "20"
+print("El valor 2 es: " + str(numero_cadena2) + "")
+suma_cadenas = numero_cadena1 + numero_cadena2
+print("La suma de los valores es: " + suma_cadenas)
+numero_entero1 = int(numero_cadena1)
+numero_entero2 = int(numero_cadena2)
+suma_enteros = numero_entero1 + numero_entero2
+print("La suma de los valores es: " + str(suma_enteros))
