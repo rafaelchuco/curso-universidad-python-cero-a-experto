7dc55678fe8ff7b480d157aaf75cf0ebdb06b501
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-05-22 19:50:23 -0500

    add receta-cocina-reto.py

---
+++ o5. Entrada  de Datos por Consola/receta-cocina-reto.py
@@ -0,0 +1,12 @@
+print("*** Receta de Cocina ***")
+nombre = input("ingrese el nombre de la receta: ")
+ingrediente = input("Ingrese los ingredientes: ")
+tiempo = int(input("¿Cuánto tiempo tarda la preparación(min): "))
+dificultad = input("¿Cuál es la dificultad(facil, medio, dificil): ")
+
+print("---------------------------------")
+print("Receta de la receta: ", nombre)
+print("Ingredientes: ", ingrediente)
+print("Tiempo de preparación: ", tiempo, "min")
+print("Dificultad: ", dificultad)
+
