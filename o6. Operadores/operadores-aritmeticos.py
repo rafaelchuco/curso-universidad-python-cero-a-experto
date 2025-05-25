b617b83599d4aa840c01f8ff3e3fb9a71f84b6a0
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-05-24 21:52:17 -0500

    Add operadores-aritmeticos.py

---
+++ o6. Operadores/operadores-aritmeticos.py
@@ -0,0 +1,17 @@
+# Operadores aritmeticos
+a = 10
+b = 3
+
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+modulo = a % b
+potencia = a ** b
+
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+print(modulo)
+print(potencia)
