e8d98f3c5c0142112a8cce017fda9bfbad5b5464
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-05-24 21:56:15 -0500

    Add operadores-aritmeticos.py and operadores_asignacion.py

---
+++ o6. Operadores/operadores_asignacion.py
@@ -0,0 +1,9 @@
+print ("***Operadores de asignacion****")
+numero = 5
+print(f"El numero es {numero}")
+numero = 10
+print(f"El numero es {numero}")
+cadena = "Python"
+print(f"La cadena es {cadena}")
+cadena = "Java"
+print(f"La cadena es {cadena}")
