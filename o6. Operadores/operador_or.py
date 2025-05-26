f9d45c4bf51c9cb1d501454d01ebcb2a2050a8bb
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-05-25 21:32:44 -0500

    Update sistema-descuento-vip-and.py

---
+++ o6. Operadores/operador_or.py
@@ -0,0 +1,17 @@
+print("*** Operador logico OR")
+
+# Regresa verdadero si una o ambas condiciones son verdaderas
+
+condicion1 = False
+condicion2 = True
+resultado = condicion1 or condicion2
+
+print(resultado)
+
+# Regresa falso si ambas condiciones son falsa
+condicion1 = False
+condicion2 = False
+
+resultado = condicion1 or condicion2
+
+print(resultado)
