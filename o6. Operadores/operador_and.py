921a4a9648468bc8a45d27baddcdaa9dcdd91141
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-05-25 21:21:33 -0500

    Add operador_and.py

---
+++ o6. Operadores/operador_and.py
@@ -0,0 +1,15 @@
+print("*** Operador AND ***")
+# Regresa verdadero si ambas condiciones son verdaderas
+condicion1 = True
+condicion2 = True
+resultado = condicion1 and condicion2
+
+print(resultado)
+
+# Regresa falso si una condición es falsa
+condicion1 = False
+condicion2 = True
+resultado = condicion1 and condicion2
+
+print(resultado)
+
