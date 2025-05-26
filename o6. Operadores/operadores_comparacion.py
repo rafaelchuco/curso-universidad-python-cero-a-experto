6ac4f3c768fa9d6bbe9fdedc80303302fbde0ffb
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-05-25 21:16:58 -0500

    Add operadores_comparacion.py

---
+++ o6. Operadores/operadores_comparacion.py
@@ -0,0 +1,22 @@
+print("***Operadores de comparación***")
+a,b = 7,5
+print(f"El valor de a es {a} y el valor de b es {b}")
+
+# Operador de comparación ==
+print(f"El valor de a es igual al valor de b: {a==b}")
+
+# Operador de comparación !=
+print(f"El valor de a es diferente al valor de b: {a!=b}")
+
+# Operador de comparación >
+print(f"El valor de a es mayor que el valor de b: {a>b}")
+
+# Operador de comparación <
+print(f"El valor de a es menor que el valor de b: {a<b}")
+
+# Operador de comparación >=
+print(f"El valor de a es mayor o igual que el valor de b: {a>=b}")
+
+# Operador de comparación <=
+print(f"El valor de a es menor o igual que el valor de b: {a<=b}")
+
