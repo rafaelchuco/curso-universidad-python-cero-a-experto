b164b94ad0a1ed62434f14d685fc6d37bb6e9fdf
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-05-25 21:09:51 -0500

    Add operadores_asignacion_compuesto.py

---
+++ o6. Operadores/operadores_asignacion_compuesto.py
@@ -0,0 +1,22 @@
+print("***Operadores de asignacion compuesto***")
+a,b = 10,15
+print(f"El valor inicial de a es {a} y el valor inicial de b es {b}")
+
+# Operador compuesto de suma +=
+a += b
+print(f"El valor final de a es {a} y el valor final de b es {b}")
+
+# Operador compuesto de resta -=
+a,b = 10,15
+a -= b
+print(f"El valor final de a es {a} y el valor final de b es {b}")
+
+# Operador compuesto de multiplicación *=
+a,b = 10,15
+a *= b
+print(f"El valor final de a es {a} y el valor final de b es {b}")
+
+# Operador compuesto de división /=
+a,b = 10,15
+a /= b
+print(f"El valor final de a es {a} y el valor final de b es {b}")
