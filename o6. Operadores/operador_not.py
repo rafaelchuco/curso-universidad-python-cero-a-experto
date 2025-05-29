f5315a9bf97d4ef4c2f784d95090efb0a7ebf8aa
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-05-29 18:23:10 -0500

    Add operador_not.py

---
+++ o6. Operadores/operador_not.py
@@ -0,0 +1,16 @@
+print("*** Operador NOT ***")
+condicion1 = False
+print(f"La condicion es: {condicion1}")
+resultado = not condicion1
+print(f"La negacion de la condicion es: {resultado}")
+
+
+# Revisar si una variable es cadena vacia
+nombre = "juan"
+es_vacia = not nombre
+print(f"La variable {nombre} es vacia: {es_vacia}")
+
+# Revisar si una variable no tiene ningun valor asignado
+variable = None
+es_vacia = not variable
+print(f"La variable {variable} es vacia: {es_vacia}")
